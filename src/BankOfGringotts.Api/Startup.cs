07dc8cd497f105a892ebd3ec165eb43c50d08d94
Hasan Basaran <Hasan.Basaran@morhipo.com>
2022-04-17 19:52:37 +0300

    exception and swagger

--- src/BankOfGringotts.Api/Startup.cs
+++ src/BankOfGringotts.Api/Startup.cs
@@ -81,12 +81,12 @@ namespace BankOfGringotts.Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+            //if (env.IsDevelopment())
+            //{
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BankOfGringotts.Api v1"));
-            }
+            //}
 
             app.UseHttpsRedirection();
 
